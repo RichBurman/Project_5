871bb71f6bc1ac6c2683ad39f9704eefb93f8a47
RichBurman <>
2023-10-19 12:21:10 +0000

    Add html templates for walking packages, about us, gallery and reviews and link to nav bar

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('walking-packages/', views.walking_packages, name='walking_packages'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('reviews/', views.reviews, name='reviews'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,19 @@ def index(request):
     """ Return the index page """
 
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    return render(request, 'home/about_us.html')
+
+
+def gallery(request):
+    return render(request, 'home/gallery.html')
+
+
+def reviews(request):
+    return render(request, 'home/reviews.html')
+
+
+def walking_packages(request):
+    return render(request, 'home/walking_packages.html')
