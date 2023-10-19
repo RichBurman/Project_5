ad134b876d07ba92fd0faf02c6aa783dc0fe2f56
RichBurman <>
2023-10-19 08:26:59 +0000

    Create user app, models for user and installed CountryField

---
+++ user/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.countries.fields import CountryField
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    addressline_1 = models.CharField(max_length=50, blank=True)
+    addressline_2 = models.CharField(max_length=50, blank=True)
+    default_county = models.CharField(max_length=30, blank=True)
+    default_postcode = models.CharField(max_length=10, blank=True)
+    default_country = CountryField(blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def__str__(self):
+        return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+
